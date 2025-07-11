aacdd38ae3dc26918b6e3d1b261be85f1feeb37c
Marlon Oliveira <marlonsantanaoliveira2007@gmail.com>
2025-07-11 17:14:13 -0400

    comecando criacao do component de input

---
+++ components/ui/input-component.tsx
@@ -0,0 +1,10 @@
+import { TextInput, Image } from "react-native";
+
+export default function InputComponent({placeholder, Image}){
+    return (
+        <TextInput
+            placeholder=""
+            className=""
+        />
+    )
+}
