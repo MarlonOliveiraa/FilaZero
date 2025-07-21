b352e2098eafeefafd59e47cceb43d962083a9b6
Marlon Oliveira <marlonsantanaoliveira2007@gmail.com>
2025-07-21 17:13:20 -0400

    criado estrutura inicial do menu

---
+++ components/ui/menu-component.tsx
@@ -0,0 +1,14 @@
+import { View, Image } from "react-native";
+
+type MenuProps = {
+    icon: any;
+}
+
+export default function MenuComponent({icon}: MenuProps){
+    const icons = []
+
+
+    <View>
+  
+    </View>
+}
