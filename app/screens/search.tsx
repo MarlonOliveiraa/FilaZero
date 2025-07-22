14815381bfb49229fe331d38486b9987a203f0a5
Marlon Oliveira <marlonsantanaoliveira2007@gmail.com>
2025-07-22 17:10:16 -0400

    estrutura inicial

--- app/screens/search.tsx
+++ app/screens/search.tsx
@@ -1,46 +1,49 @@
 import InputComponent from "@/components/ui/input-component";
-import Menucomponent from "@/components/ui/menu-component";
-import { useThemeColor } from '@/hooks/useThemeColor';
+import { useThemeColor } from "@/hooks/useThemeColor";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const menuItems = [
-        { icon: require('@/assets/icons/home.png'), route: 'screens/home' }, 
-        { icon: require('@/assets/icons/search.png'), route: 'screens/search' }, 
-        { icon: require('@/assets/icons/fila.png'), route: 'screens/filas' }, 
-        { icon: require('@/assets/icons/profile.png'), route: 'screens/profile' }, 
-    ]
+  { icon: require("@/assets/icons/home.png"), route: "screens/home" },
+  { icon: require("@/assets/icons/search.png"), route: "screens/search" },
+  { icon: require("@/assets/icons/fila.png"), route: "screens/filas" },
+  { icon: require("@/assets/icons/profile.png"), route: "screens/profile" },
+];
 
 export default function Filas() {
-    const background = useThemeColor('background');
-    const text = useThemeColor('text');
-    
-    return (
-        <View style={[styles.container, { backgroundColor: background}]}>
-            <Text>Filas</Text>
+  const background = useThemeColor("background");
+  const text = useThemeColor("text");
 
-            <Menucomponent 
-                items={menuItems}
-            />
-        </View>
-    );
+  return (
+    <View>
+      <TouchableOpacity>
+        <Image source={require("@/assets/icons/arrowBack.png")} />
+      </TouchableOpacity>
+      <InputComponent
+        placeholder="Pesquise a empresa"
+        keyboardType="email-address"
+        icon={require("@/assets/icons/search-dark.png")}
+      />
+      <Text style={styles.text}>Empresas parceiras</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create ({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: 16,
-    },
-    text: {
-        fontSize: 48,
-        fontFamily: 'PoppinsRegular',
-        lineHeight: 60,
-    },
-    subtitle: {
-        fontSize: 12,
-        fontFamily: 'PoppinsRegular',
-        marginBottom: 72,
-    },
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 16,
+  },
+  text: {
+    fontSize: 20,
+    fontFamily: "PoppinsRegular",
+    lineHeight: 60,
+  },
+  subtitle: {
+    fontSize: 12,
+    fontFamily: "PoppinsRegular",
+    marginBottom: 72,
+  },
 });
