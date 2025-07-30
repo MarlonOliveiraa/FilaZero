21be02828318bdd8b048554bd38f09e09893d936
Marlon Oliveira <marlonsantanaoliveira2007@gmail.com>
2025-07-30 16:58:41 -0400

    removendo coments

--- backend/public/index.php
+++ backend/public/index.php
@@ -19,23 +19,20 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
 
-// Remove basePath
 if (strpos($uri, $basePath) === 0) {
     $uri = substr($uri, strlen($basePath));
 }
 
-// Remove /index.php se existir no começo
 if (strpos($uri, '/index.php') === 0) {
     $uri = substr($uri, strlen('/index.php'));
 }
 
-// Limpa espaços e garante '/' se estiver vazio
 $uri = trim($uri);
 if ($uri === '') {
     $uri = '/';
 }
 
-// 
+
 if ($method === 'POST' && $uri === '/register') {
     $auth->register();
     exit;
@@ -44,9 +41,8 @@ if ($method === 'POST' && $uri === '/register') {
 if ($method === 'POST' && $uri === '/login') {
     $auth->login();
     exit;
-}
+} 
 
-// Se nenhuma rota for encontrada
 http_response_code(404);
 echo json_encode(['message' => 'Rota não encontrada', 'uri' => $uri]);
 exit;
