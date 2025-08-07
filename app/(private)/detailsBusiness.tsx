70c7c2a2887a5962c2326ca415585b8d57c86cd4
Marlon Oliveira <marlonsantanaoliveira2007@gmail.com>
2025-08-07 14:11:47 -0400

    att

--- app/(private)/detailsBusiness.tsx
+++ app/(private)/detailsBusiness.tsx
@@ -4,7 +4,14 @@ import PerfilComponent from "@/components/ui/perfil-component";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import React from "react";
-import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 const menuItems = [
   { icon: require("@/assets/icons/home.png"), route: "/(private)/home" },
@@ -33,10 +40,12 @@ const dadosMockados: Empresa[] = [
   },
   {
     id: 2,
-    nome: "Clínica Popular",
-    descricao: "Consultas e exames com preço acessível.",
-    endereco: "Rua Bahia, 456 - Popular, Campo Grande - MS",
-    image: "https://t.ctcdn.com.br/T3vA3dToBndK4WbU1K6_6j-IyD0=/400x400/smart/i490109.jpeg",
+    nome: "Caixa Econômica Federal - Agência Centro",
+    descricao:
+      "Agência movimentada com atendimentos para FGTS, Bolsa Família, habitação e serviços bancários em geral.",
+    endereco: "Rua 14 de Julho, 123 - Centro, Campo Grande - MS",
+    image:
+      "https://t.ctcdn.com.br/T3vA3dToBndK4WbU1K6_6j-IyD0=/400x400/smart/i490109.jpeg",
     iconRedirect: require("@/assets/icons/external-link.png"),
   },
   {
@@ -51,8 +60,10 @@ const dadosMockados: Empresa[] = [
     id: 4,
     nome: "Laboratório Alfa",
     descricao: "Exames laboratoriais com agendamento online.",
-    endereco: "Av Ernesto Geisel, 321 - Bairro São Francisco, Campo Grande - MS",
-    image: "https://t.ctcdn.com.br/55l-VCvM_3sCA7Ha9VHzsnZsKQk=/600x600/smart/i535351.jpeg",
+    endereco:
+      "Av Ernesto Geisel, 321 - Bairro São Francisco, Campo Grande - MS",
+    image:
+      "https://t.ctcdn.com.br/55l-VCvM_3sCA7Ha9VHzsnZsKQk=/600x600/smart/i535351.jpeg",
     iconRedirect: require("@/assets/icons/external-link.png"),
   },
 ];
@@ -82,148 +93,150 @@ export default function DetailBusiness() {
 
   return (
     <View style={[styles.container, { backgroundColor: background }]}>
-        <ScrollView
+      <ScrollView
         contentContainerStyle={styles.scrollContent}
         showsVerticalScrollIndicator={false}
       >
         <View style={{ width: "90%", alignItems: "flex-start" }}>
-            <TouchableOpacity
+          <TouchableOpacity
             style={{ flex: 1, paddingTop: 30 }}
             onPress={() => router.push("/(private)/filas")}
-            >
+          >
             <Image source={require("@/assets/icons/arrowBack.png")} />
-            </TouchableOpacity>
+          </TouchableOpacity>
         </View>
 
         <PerfilComponent
-            image={
+          image={
             typeof empresa.image === "string"
-                ? { uri: empresa.image }
-                : empresa.image
-            }
+              ? { uri: empresa.image }
+              : empresa.image
+          }
         />
 
         <View style={{ width: "90%" }}>
-            <Text
+          <Text
             style={[
-                { fontWeight: "bold", fontSize: 20, marginTop: 8 },
-                { color: text },
+              { fontWeight: "bold", fontSize: 20, marginTop: 8 },
+              { color: text },
             ]}
-            >
+          >
             {empresa.nome}
-            </Text>
+          </Text>
 
-            <Text
+          <Text
             style={[
-                {
+              {
                 fontWeight: "400",
                 fontSize: 16,
                 marginTop: 8,
                 paddingRight: 32,
-                },
-                { color: text },
+              },
+              { color: text },
             ]}
-            >
+          >
             {empresa.descricao}
-            </Text>
+          </Text>
 
-            <Text
+          <Text
             style={[
-                {
+              {
                 fontWeight: "400",
                 fontSize: 16,
                 marginTop: 32,
                 paddingRight: 64,
-                },
-                { color: text },
+              },
+              { color: text },
             ]}
-            >
+          >
             {empresa.endereco}
-            </Text>
+          </Text>
         </View>
 
         <View
-            style={{
+          style={{
             width: "90%",
             justifyContent: "center",
             alignItems: "flex-start",
             gap: 16,
-            }}
+          }}
         >
-            <View
+          <View
             style={{
-                flex: 1,
-                flexDirection: "row",
-                width: "100%",
-                gap: 12,
-                alignItems: "center",
+              flex: 1,
+              flexDirection: "row",
+              width: "100%",
+              gap: 12,
+              alignItems: "center",
             }}
-            >
+          >
             <Image source={require("@/assets/icons/phone.png")} />
             <Text style={{ fontWeight: "bold" }}>(67) 99100-9862</Text>
-            </View>
+          </View>
 
-            <View
+          <View
             style={{
-                flex: 1,
-                flexDirection: "row",
-                width: "100%",
-                gap: 12,
-                alignItems: "center",
+              flex: 1,
+              flexDirection: "row",
+              width: "100%",
+              gap: 12,
+              alignItems: "center",
             }}
-            >
+          >
             <Image source={require("@/assets/icons/time.png")} />
-            <Text style={{ fontWeight: "bold", color: "#ff0000" }}>Fechado</Text>
+            <Text style={{ fontWeight: "bold", color: "#ff0000" }}>
+              Fechado
+            </Text>
             <View
-                style={{
+              style={{
                 borderRadius: 100,
                 width: 8,
                 height: 8,
                 borderColor: "#1d1d1dff",
                 borderWidth: 3,
-                }}
+              }}
             />
             <Text style={{ fontWeight: "600", color: "#21212198" }}>
-                Abre às 8 horas
+              Abre às 8 horas
             </Text>
             <Image source={require("@/assets/icons/arrow-down-fill.png")} />
-            </View>
+          </View>
 
-            <View style={{ flexDirection: "row", gap: 16 }}>
+          <View style={{ flexDirection: "row", gap: 16 }}>
             <TouchableOpacity
-                onPress={() => router.push("https://wa.me/5567991009862")}
+              onPress={() => router.push("https://wa.me/5567991009862")}
             >
-                <Image source={require("@/assets/icons/whatsapp-icon.png")} />
+              <Image source={require("@/assets/icons/whatsapp-icon.png")} />
             </TouchableOpacity>
             <TouchableOpacity
-                onPress={() => router.push("https://instagram.com/evandroocm")}
+              onPress={() => router.push("https://instagram.com/evandroocm")}
             >
-                <Image source={require("@/assets/icons/instagram-icon.png")} />
+              <Image source={require("@/assets/icons/instagram-icon.png")} />
             </TouchableOpacity>
             <TouchableOpacity
-                onPress={() => router.push("https://facebook.com/evandromktd")}
+              onPress={() => router.push("https://facebook.com/evandromktd")}
             >
-                <Image source={require("@/assets/icons/facebook-icon.png")} />
+              <Image source={require("@/assets/icons/facebook-icon.png")} />
             </TouchableOpacity>
-            </View>
+          </View>
         </View>
 
         <View
-            style={{
+          style={{
             width: "100%",
             justifyContent: "center",
             alignItems: "center",
             marginTop: 32,
-            }}
+          }}
         >
-            <BotaoComponent
+          <BotaoComponent
             titulo="Retirar senha digital"
             corFundo="background2"
             corTexto="text2"
             funcao={() => router.push("/(auth)/confirmPassword")}
-            />
+          />
         </View>
-        </ScrollView>
+      </ScrollView>
 
       <Menucomponent items={menuItems} />
     </View>
